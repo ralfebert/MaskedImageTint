61107000840594c6f05a8a43d48dd425465c94ac
Ralf Ebert <info@ralfebert.de>
2020-03-03 14:32:22 +0100

    comments

--- MaskedImageTint.playground/Contents.swift
+++ MaskedImageTint.playground/Contents.swift
@@ -7,15 +7,25 @@ let imageRenderer = UIGraphicsImageRenderer(size: image.size)
 
 let tintedImage = imageRenderer.image { (ctx) in
     let cgContext = ctx.cgContext
+    
+    // draw original image
     let rect = CGRect(origin: .zero, size: image.size)
     image.draw(in: rect)
+    
+    // save state to restore the clipping mask
     cgContext.saveGState()
+    
+    // apply clipping mask accounting for CGImages drawn mirrored
     cgContext.translateBy(x: 0, y: rect.size.height)
     cgContext.scaleBy(x: 1.0, y: -1.0);
     cgContext.clip(to: rect, mask: mask.cgImage!)
+    
+    // fill with mask applied
     cgContext.setBlendMode(.color)
     UIColor.red.setFill()
     cgContext.fill(rect)
+    
+    // remove clipping mask
     cgContext.restoreGState()
 }
 
